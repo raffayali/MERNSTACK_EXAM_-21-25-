21f78e4cf69f26af44145f90c327523006d4ac29
raffayali <raffayalia5535@gmail.com>
2023-10-28 17:21:54 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Api from './api';
+
+function App() {
+  return (
+    <div className="App">
+      <Api/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
+function api() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/albums')
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>LOADING ........</h2>
+      {loading ? (
+        <p>PLEASE WAIT BRO</p>
+      ) : (
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default api;
